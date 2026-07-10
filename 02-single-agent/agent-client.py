58a432cffc9978df957043e106068ed7aa3d66b2
Karolina Śnieżek <karolina.sniezek@xebia.com>
2026-07-10 22:07:09 +0200

    add simple agent

--- 02-single-agent/agent-client.py
+++ 02-single-agent/agent-client.py
@@ -3,33 +3,33 @@ import os
 from dotenv import load_dotenv
 from azure.identity import DefaultAzureCredential
 from azure.ai.projects import AIProjectClient
+
 load_dotenv()
 
-project_endpoint = os.getenv("PROJECT_ENDPOINT")
+PROJECT_ENDPOINT = os.getenv("PROJECT_ENDPOINT")
+AGENT_NAME = os.getenv("AGENT_NAME")
 
 with (
     DefaultAzureCredential() as credential,
     AIProjectClient(
-        endpoint=project_endpoint,
+        endpoint=PROJECT_ENDPOINT,
         credential=credential,
+        allow_preview=True,
     ) as project_client,
 ):
     print("✅ Connected to Azure AI Foundry!")
 
-    openai_client = project_client.get_openai_client()
+    agent = project_client.agents.get(agent_name=AGENT_NAME)
 
-    print("✅ OpenAI client created!")
+    agent_version = agent.versions["latest"].version
+    print(f"Using version: {agent_version}")
 
-openai_client = project_client.get_openai_client()
+    openai_client = project_client.get_openai_client(
+        agent_name=AGENT_NAME
+    )
 
-response = openai_client.chat.completions.create(
-    model="gpt-5.4-mini",
-    messages=[
-        {
-            "role": "user",
-            "content": "Explain Azure AI Foundry in one sentence."
-        }
-    ]
-)
+    response = openai_client.responses.create(
+        input="What is Azure AI Foundry?"
+    )
 
-print(response.choices[0].message.content)
+    print(response.output_text)
